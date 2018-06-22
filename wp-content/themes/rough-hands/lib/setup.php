b6e49ae751d6dd394997e285e6e893ad47618d89
Shane <swalkergibson@gmail.com>
2018-06-22 15:04:47 -0700

    Theming
    * Adding images to home page
    * Adding SlickJS dependency for slider

--- wp-content/themes/rough-hands/lib/setup.php
+++ wp-content/themes/rough-hands/lib/setup.php
@@ -98,6 +98,7 @@ function display_sidebar() {
  */
 function assets() {
   wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+  wp_enqueue_style('slick', Assets\asset_path('styles/slick.css'), false, null);
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
@@ -105,5 +106,6 @@ function assets() {
 
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
   wp_enqueue_script('retina', Assets\asset_path('scripts/retina.min.js'), [], null, true);
+  wp_enqueue_script('slick', Assets\asset_path('scripts/slick.min.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
