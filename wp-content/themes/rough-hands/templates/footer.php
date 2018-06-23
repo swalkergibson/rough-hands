94565ac5f343b9b77ce29de9ce0daefd5bade3fb
Shane <swalkergibson@gmail.com>
2018-06-23 13:58:13 -0700

    Theming
    * Adding fonts
    * Styling text
    * Begin footer styling

--- wp-content/themes/rough-hands/templates/footer.php
+++ wp-content/themes/rough-hands/templates/footer.php
@@ -2,7 +2,7 @@
 <div class="jumbotron mb-0">
     <div class="container">
         <div class="row">
-            <div class="col-xs-6 col-md-2">
+            <div class="col-xs-6 col-md-2 text-center">
                 <img src="/wp-content/themes/rough-hands/dist/images/location_img_invert.png" alt="Barbershop location image" />
             </div>
             <div class="col-xs-6 col-md-3">
