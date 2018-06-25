3a2fa60f19c90afc6027f194d464db17cdc922c4
Shane <swalkergibson@gmail.com>
2018-06-25 15:54:10 -0700

    Theming
    * Positioning main content container
    * Developing custom navigation menu

--- wp-content/themes/rough-hands/templates/head.php
+++ wp-content/themes/rough-hands/templates/head.php
@@ -2,7 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
 
   <!-- Inject hero image into page CSS, if it exists -->
   <?php if (has_post_thumbnail($post)) : $hero = get_the_post_thumbnail_url($post, 'full'); ?>
@@ -12,4 +11,6 @@
       }
     </style>
   <?php endif; ?>
+
+  <?php wp_head(); ?>
 </head>
--- wp-content/themes/rough-hands/templates/header.php
+++ wp-content/themes/rough-hands/templates/header.php
@@ -1,10 +1,18 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+  <div class="container text-center">
+    <!--<a class="brand" href="<?= esc_url(home_url('/')); ?>"><img src="/wp-content/themes/rough-hands/dist/images/logo_long.png" alt="Rough Hands Barbershop logo" /></a>-->
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        $items = wp_get_nav_menu_items('Main Menu');
+        foreach ($items as $item) {
+            if ($item->title !== 'Logo') {
+                $item->class = 'text-link';
+            }
+        }
+        $main = wp_nav_menu(['menu' => $items, 'echo' => false, 'theme_location' => 'primary_navigation', 'menu_class' => 'nav justify-content-center']);
+        $main = str_replace('Logo', '<img src="/wp-content/themes/rough-hands/dist/images/logo_long.png" />', $main);
+        echo $main;
       endif;
       ?>
     </nav>
