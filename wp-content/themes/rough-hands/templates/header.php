af5d950b7ea23e0042b7b3b533fe7755ad278a31
Shane <swalkergibson@gmail.com>
2018-06-26 20:03:17 -0700

    Theming
    * Clean up commented code

--- wp-content/themes/rough-hands/templates/header.php
+++ wp-content/themes/rough-hands/templates/header.php
@@ -1,25 +1,3 @@
-<?php /*
-<header class="banner">
-  <div class="container text-center">
-    <!--<a class="brand" href="<?= esc_url(home_url('/')); ?>"><img src="/wp-content/themes/rough-hands/dist/images/logo_long.png" alt="Rough Hands Barbershop logo" /></a>-->
-    <nav class="navbar navbar-dark nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        $items = wp_get_nav_menu_items('Main Menu');
-        foreach ($items as $item) {
-            if ($item->title !== 'Logo') {
-                $item->class = 'text-link';
-            }
-        }
-        $main = wp_nav_menu(['menu' => $items, 'echo' => false, 'theme_location' => 'primary_navigation', 'menu_class' => 'navbar-nav nav justify-content-center']);
-        $main = str_replace('Logo', '<img src="/wp-content/themes/rough-hands/dist/images/logo_long.png" />', $main);
-        echo $main;
-      endif;
-      ?>
-    </nav>
-  </div>
-</header>
-*/ ?>
 <header class="banner">
   <div class="container">
     <nav class="navbar navbar-dark bg-dark nav-primary navbar-expand-md bg-transparent">
